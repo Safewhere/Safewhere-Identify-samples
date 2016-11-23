5a0768d68aa277afa54639e49d9cbbef97c18a62
phuong le <letranxuanphuong@gmail.com>
2016-11-23 20:06:22 +0700

    #30255 DA - STS Generic Provider functionality

---
+++ Safewhere.Samples.STS/Safewhere.Samples.STS.GenericCredentialsValidator/UsernamePasswordGenericCredentialsValidator.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Safewhere.External;
+using Safewhere.External.Authentication;
+
+namespace Safewhere.Samples.STS.GenericCredentialsValidator
+{
+    public class UsernamePasswordGenericCredentialsValidator : IGenericCredentialsValidator
+    {
+        const string ServiceIdentifier = "serviceIdentifier";
+        const string UserName = "username";
+        const string Password = "password";
+        const string AdditionalClaims = "additionalclaim";
+        /// <summary>
+        /// Validate credentials by generic provider
+        /// </summary>
+        /// <param name="cc">Notice that on STs the controller context is irrelevant</param>
+        /// <param name="inputs"></param>
+        /// <param name="logWriter"></param>
+        /// <returns></returns>
+        public CredentialsValidationResult Validate(ControllerContext cc, IDictionary<string, string> inputs, IIdentifyLogWriter logWriter)
+        {
+            var validationResult = ValidateInput(inputs);
+
+            if (validationResult.ResultCode != CredentialsValidationResultCode.Success)
+            {
+                logWriter.WriteError("Validate the input failed. Error code: " + validationResult.ResultCode +
+                                       ". ExternalErrorMessages: " + string.Join("\n-", validationResult.ExternalErrorMessages));
+                validationResult.ShowErrorViewWhenResultCodeIsNotSuccess = false;
+                return validationResult;
+            }
+            var username = inputs[UserName];
+            var serviceIdentifier = inputs[ServiceIdentifier];
+            var logResult = new StringBuilder();
+            logResult.AppendLine(
+                $"Successfully validate generic credentials for username = '{username}' with service identifier ='{serviceIdentifier}'");
+            logResult.AppendLine("");
+
+            foreach (var input in inputs)
+            {
+                if (input.Key.StartsWith(AdditionalClaims, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    logResult.AppendLine($"Additional claims received: type = '{input.Key}' - value ='{input.Value}'");
+                }
+            }
+            logWriter.WriteInformation(logResult); 
+
+            return new CredentialsValidationResult {ResultCode = CredentialsValidationResultCode.Success};
+        }
+        private static CredentialsValidationResult ValidateInput(IDictionary<string, string> inputs)
+        {
+            CredentialsValidationResult validationResult = new CredentialsValidationResult();
+            if (!inputs.ContainsKey(ServiceIdentifier))
+            {
+                validationResult.ExternalErrorMessages.Add("Missing connection string field");
+                validationResult.ResultCode = CredentialsValidationResultCode.MissingRequiredFields;
+                return validationResult;
+            }
+
+            if (!inputs.ContainsKey(UserName))
+            {
+                validationResult.ExternalErrorMessages.Add("Missing username field");
+                validationResult.ResultCode = CredentialsValidationResultCode.MissingRequiredFields;
+                return validationResult;
+            }
+
+            if (!inputs.ContainsKey(Password))
+            {
+                validationResult.ExternalErrorMessages.Add("Missing password field.");
+                validationResult.ResultCode = CredentialsValidationResultCode.MissingRequiredFields;
+                return validationResult;
+            }
+
+            validationResult.ResultCode = CredentialsValidationResultCode.Success;
+            return validationResult;
+        }
+    }
+}
